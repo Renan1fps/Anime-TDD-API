6de896fae79056e908cc5c2f006771b93241fd57
MbRenan <renan.sergipe68@gmail.com>
2022-03-20 19:04:08 -0300

    test: ensure CreateAnimeController returns 400 if no name is provided

---
+++ src/presentation/controller/createAnime.spec.ts
@@ -0,0 +1,25 @@
+import { CreateAnimeController } from './createAnime'
+
+describe('CreateAnime Controller', () => {
+
+  const makeSut = () => {
+    const sut = new CreateAnimeController()
+    return {
+      sut
+    }
+  }
+
+  test('Should return 400 if no name is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        description: "any_description",
+        price: 0.00,
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error("missing param name"))
+  })
+})
