6c75c5f5e3eacc325b01f1dc918ec122eeb30305
MbRenan <renan.sergipe68@gmail.com>
2022-03-21 22:43:42 -0300

    feat: ensure CreateAnimeController returns 400 if no description is provided

--- src/presentation/controller/createAnime.ts
+++ src/presentation/controller/createAnime.ts
@@ -1,8 +1,19 @@
 export class CreateAnimeController {
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error("missing param name")
+
+    if(!httpRequest.body.name){
+      return {
+        statusCode: 400,
+        body: new Error("missing param: name")
+      }
     }
+
+    if(!httpRequest.body.description){
+      return {
+        statusCode: 400,
+        body: new Error("missing param: description")
+      }
+    }
+ 
   }
 }
