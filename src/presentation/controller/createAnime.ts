47893a9927374e1af826cfa04fe8edc9e25fe256
MbRenan <renan.sergipe68@gmail.com>
2022-03-26 19:44:49 -0300

    feat: ensure CreateAnimeController return 400 if invalid date is provided

--- src/presentation/controller/createAnime.ts
+++ src/presentation/controller/createAnime.ts
@@ -1,6 +1,10 @@
 import { badRequest } from '../helpers/httpHelper'
+import { IDateValidator } from './protocols/date-validator'
 import { IHttpRequest, IHttpResponse } from './protocols/http'
 export class CreateAnimeController {
+
+  constructor(private readonly dateValidator: IDateValidator){}
+
   handle(httpRequest: IHttpRequest): IHttpResponse {
 
     const requiredFields = ["name", "description", "price", "date"]
@@ -9,5 +13,12 @@ export class CreateAnimeController {
         return badRequest(new Error(`missing param: ${field}`))
       }
     }
+     const isValidDate = this.dateValidator.isValid(httpRequest.body.date)
+     if(!isValidDate){
+       return {
+         statusCode: 400,
+         body: new Error('invalid date')
+       }
+     }
   }
 }
