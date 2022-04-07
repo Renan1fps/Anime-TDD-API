41ce70dc89f16ee20c2aacd2d9786ddc6bcd9d03
MbRenan <renan.sergipe68@gmail.com>
2022-04-06 22:27:20 -0300

    test: ensure DateValidator returns true if valid date is provided

--- src/utils/adapter/date-validator-adpter.spec.ts
+++ src/utils/adapter/date-validator-adpter.spec.ts
@@ -1,13 +1,26 @@
+import moment from "moment"
 import { DateValidator } from "./date-validator-adapter"
 
 describe('DateValidator', () => {
 
   const date = new Date()
 
+  jest.mock('moment', () => ({
+    isDate: () => {
+      return true
+    }
+  }))
+
   test('Should return false if invalid date is provided', () => {
-    const sut =  new DateValidator()
+    const sut = new DateValidator()
     jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
     const isValid = sut.isValid(date)
     expect(isValid).toBe(false)
+  })
+
+  test('Should return false if valid date is provided', () => {
+    const sut = new DateValidator()
+    const isValid = sut.isValid(date)
+    expect(isValid).toBe(true)
   })
 })
