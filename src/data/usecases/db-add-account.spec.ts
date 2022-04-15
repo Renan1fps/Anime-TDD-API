25a730107493728d7e273aeac40f9175e8645d09
MbRenan <renan.sergipe68@gmail.com>
2022-04-15 10:56:25 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -1,27 +1,44 @@
+import { ParseDate } from "../protocols/parse-date";
 import { DbAddAnime } from "./db-add-account"
 
-describe('DbAddAnime Usecase', () => {
+const validDate = new Date()
 
-  const validDate = new Date()
- 
-test('Should call parse with correct date', async () => {
-  class ParseStub {
+const makeParse = (): ParseDate => {
+  class ParseStub implements ParseDate {
     parse(value: Date): string {
       return value.toString();
     }
   }
-  const parseStub = new ParseStub()
+
+  return new ParseStub()
+}
+
+interface ISutTypes {
+  sut: DbAddAnime,
+  parseStub: ParseDate,
+}
+
+const makeSut = (): ISutTypes => {
+  const parseStub = makeParse()
   const sut = new DbAddAnime(parseStub)
-  const isSpy = jest.spyOn(parseStub, 'parse')
-  const anime = {
-    name: 'valid_name',
-    description: 'valid_description',
-    price: 1,
-    date: validDate,
+
+  return {
+    sut,
+    parseStub,
   }
-  await sut.add(anime)
-  expect(isSpy).toHaveBeenCalledWith(validDate)
-})
+}
 
-
+describe('DbAddAnime Usecase', () => {
+  test('Should call parse with correct date', async () => {
+    const { sut, parseStub } = makeSut()
+    const isValidSpy = jest.spyOn(parseStub, 'parse')
+    const anime = {
+      name: 'valid_name',
+      description: 'valid_description',
+      price: 1,
+      date: validDate,
+    }
+    await sut.add(anime)
+    expect(isValidSpy).toHaveBeenCalledWith(validDate)
+  })
 })
